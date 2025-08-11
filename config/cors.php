c2ca0add474814b145bc0b56ca9f180da5074a84
Mark Hatherly <mhatherly@mixvoip.com>
2025-08-11 12:17:26 +0200

    Modify frontend file

--- config/cors.php
+++ config/cors.php
@@ -14,35 +14,12 @@ return [
     |
     */
 
-    'paths' => [
-        'api/*',
-        'sanctum/csrf-cookie',
-        'login',
-        'register',
-        'logout',
-        'forgot-password',
-        'reset-password',
-        'email/*',
-        'user/*',
-        'profile',
-        '/'
-    ],
-
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => [
-        env('FRONTEND_URL', 'http://localhost:3000'),
-        'http://localhost:3000',
-        'http://127.0.0.1:3000'
-    ],
-
+    'allowed_origins' => ['http://localhost:3000', 'http://localhost:8080'], // Add your frontend URL
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => true,
 ];
