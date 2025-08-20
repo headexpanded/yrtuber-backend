5f9eb584d7bb1f7672d6b99d3d9fdbeeef61d592
Mark Hatherly <mhatherly@mixvoip.com>
2025-08-20 14:31:31 +0200

    Add `CollectionFollow` model
    - Define `CollectionFollow` model with `follower` and `collection` relationships.
    - Specify `fillable` attributes for managing collection follow data.

---
+++ app/Models/CollectionFollow.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class CollectionFollow extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'follower_id',
+        'collection_id',
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function follower(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'follower_id');
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function collection(): BelongsTo
+    {
+        return $this->belongsTo(Collection::class);
+    }
+}
